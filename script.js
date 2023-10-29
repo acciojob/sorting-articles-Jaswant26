d8a06f38b128c5c0155fdca13d5638c91c26f6ee
Jaswant Singh Rawat <71880220+Jaswant26@users.noreply.github.com>
2023-10-29 11:54:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+        // Function to remove certain words from a band name
+        function stripArticles(bandName) {
+            return bandName.replace(/^(a |an |the )/i, '').trim();
+        }
+
+        // Sort the bands array while ignoring specified words
+        const sortedBands = bands.sort((a, b) => stripArticles(a) > stripArticles(b) ? 1 : -1);
+
+        // Create the sorted list and add it to the webpage
+        const bandList = document.getElementById('band-list');
+        sortedBands.forEach(band => {
+            const listItem = document.createElement('li');
+            listItem.textContent = band;
+            bandList.appendChild(listItem);
+        });
